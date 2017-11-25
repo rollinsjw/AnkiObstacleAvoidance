247bdf35e5a3a6e9b669d8f846724449897ac0f0
Rui Hou <ruihouwork@gmail.com>
2017-11-25 15:23:48 -0500

    Start camera callibration code

---
+++ camera_callibration.py
@@ -0,0 +1,15 @@
+import cv2
+import cv2
+import blob
+
+SQUARE_SIZE = 0 #meters
+BOARD_HEIGHT = 9 #number of corners
+BOARD_WIDTH = 6 #number of corders
+
+def create_known_board_positions():
+  for i in BOARD_HEIGHT:
+    for j in BOARD_WIDTH:
+      
+
+
+if __name__ == "__main__":
