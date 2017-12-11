ae7d4816d8270eedc2d2e4cb910bc1248a847a64
Diane Hadley <dfh12@duke.edu>
2017-12-10 19:28:40 -0500

    Add main and rename simulation

---
+++ main.py
@@ -0,0 +1,9 @@
+from simulation import Simulation
+
+# Starts simulation
+def main():
+	simulation = Simulation()
+	simulation.stream()
+
+if __name__ == "__main__":
+	main()
---
+++ simulation.py
@@ -0,0 +1,39 @@
+import numpy as np
+import cv2
+import time
+
+from config import Config
+from object_tracking import ObjectTracking
+from undistortion import Undistortion
+
+class Simulation:
+
+	def __init__():
+		config = Config()
+		self.undistortion = Undistortion(config)
+		self.object_tracking = ObjectTracking()
+
+	def stream():
+		vid = cv2.VideoCapture(0)
+
+		# Check that capture was initialized
+		if (!vid.isOpened()):
+			vid.open()
+
+		#TODO: add way to save video
+
+		while True:
+			return_value, frame = vid.read()
+
+			if not return_value:
+				break
+
+			# Undistort image
+			undistorted_frame = self.undistortion.undistort(frame)
+
+			#TODO: call detect cars and detect obstacles from object tracking
+			#TODO: use control to change state
+			#TODO: send anki commands
+
+		vid.release()
+
