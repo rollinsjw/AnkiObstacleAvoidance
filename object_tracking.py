869ac5a80c921945379247ccc89d709bdb0908b6
Diane Hadley <dfh12@duke.edu>
2017-12-10 19:28:59 -0500

    Add classes

--- object_tracking.py
+++ object_tracking.py
@@ -2,7 +2,12 @@
 import cv2
 import numpy as np
 
-def detect_cars(frame):
-	gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	thresh_image = cv2.threshold(gray_image, 200, 255, cv2.THRESH_BINARY)[1]
+class ObjectTracking:
+
+	def detect_cars(frame):
+		gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		thresh_image = cv2.threshold(gray_image, 200, 255, cv2.THRESH_BINARY)[1]
+
+	def detect_obstacles(frame):
+		pass
 
