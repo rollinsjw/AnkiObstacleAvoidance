35cd782ccaa81a0e4b310c9722fda7bd0d446738
Diane Hadley <dfh12@duke.edu>
2017-12-13 04:58:01 -0500

    Try new bucket size

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import cv2
 
 def test():
 	image = cv2.imread("pattern.png")
-	blue_hist = cv2.calcHist([image], [0], None, [256], [0, 256])
-	green_hist = cv2.calcHist([image], [1], None, [256], [0, 256])
-	red_hist = cv2.calcHist([image], [2], None, [256], [0, 256])
+	blue_hist = cv2.calcHist([image], [0], None, [32], [0, 256])
+	green_hist = cv2.calcHist([image], [1], None, [32], [0, 256])
+	red_hist = cv2.calcHist([image], [2], None, [32], [0, 256])
 	#hist = cv2.calcHist([image], [0, 1, 2], None, [32, 32, 32], [0, 256])
 	print blue_hist
 	print green_hist
