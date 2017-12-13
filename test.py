b8ca419cacb8369390d8e44fe0395e4ea98ad98c
Diane Hadley <dfh12@duke.edu>
2017-12-13 04:27:57 -0500

    Edit test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import cv2
 
 def test():
 	image = cv2.imread("pattern.png")
-	hist = cv2.calcHist([image], [0, 1, 2], None, [32, 32, 32], [0, 256, 0, 256, 0, 256])
+	hist = cv2.calcHist([image], [0, 1], None, [32, 32], [0, 256, 0, 256])
 	#hist = cv2.calcHist([image], [0, 1, 2], None, [32, 32, 32], [0, 256])
 	print hist[0]
 	print len(hist[0])
