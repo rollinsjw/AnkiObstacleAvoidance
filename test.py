3c422c0b1c512ad1c01d7fb95892f86c8f8b1733
Diane Hadley <dfh12@duke.edu>
2017-12-13 14:23:02 -0500

    Uncomment some of test

--- test.py
+++ test.py
@@ -5,9 +5,7 @@ def test():
 	
 	original_image = cv2.imread('white_circle.jpg')
 
-
-
-	#image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+	image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
 
 	# im2, contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
