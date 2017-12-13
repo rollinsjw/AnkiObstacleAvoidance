4e69346907a4fa74071630c1e1a54f1d4eadc576
Diane Hadley <dfh12@duke.edu>
2017-12-13 03:35:51 -0500

    Fix test and add pattern image

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-
+import cv2
 
 def test():
-	image = cv2.imread("Textbooks.jpg")
+	image = cv2.imread("pattern.png")
 	hist = cv2.calcHist([image], [0, 1, 2], None, [32, 32, 32], [0, 256])
 	print hist
 
