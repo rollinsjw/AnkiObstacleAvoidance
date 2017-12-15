960d0253aa1b95dbcd9a926c9c8e0c6d7ce9d170
Diane Hadley <dfh12@duke.edu>
2017-12-15 15:42:27 -0500

    Adjust script for finding rectangles

--- test.py
+++ test.py
@@ -9,18 +9,6 @@ def test():
 
 	contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-	#print contours
-
-	#contour = contours[0]
-
-	# rect = cv2.minAreaRect(contour)
-
-	# print rect
-
-	# box = cv2.cv.boxPoints(rect)
-	# box = np.int0(box)
-	# cv2.drawContours(image,[box],0,(0,0,255),2)
-
 	max_rectangle = (0, 0, 0 , 0)
 	for i in range(len(contours)):	
 		x,y,w,h = cv2.boundingRect(contours[i])
@@ -30,12 +18,9 @@ def test():
 	x,y,w,h = max_rectangle
 	cv2.rectangle(original_image,(x,y),(x+w,y+h),(0,0,255),2)
 		
-
-
 	cv2.imshow('image', original_image)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
-
 
 
 
