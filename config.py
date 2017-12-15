9495382d36a110fc6ca3f1fb493bc53ce83e6550
Diane Hadley <dfh12@duke.edu>
2017-12-15 17:05:10 -0500

    Debug

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ import ConfigParser
 
 class Config:
 
-	def __init__():
+	def __init__(self):
 		self.config = ConfigParser.ConfigParser()
 		self.config.read('properties.ini')
 
-	def get_calibration_filename():
+	def get_calibration_filename(self):
 		return self.config.get('calibration', 'file_name')
 
-	def get_obstacle_hist_filename():
+	def get_obstacle_hist_filename(self):
 		return self.config.get('obstacle_histogram', 'file_name')
 
-	def get_obstacle_hist_bucket_size():
+	def get_obstacle_hist_bucket_size(self):
 		return int(self.config.get('obstacle_histogram', 'bucket_size'))
